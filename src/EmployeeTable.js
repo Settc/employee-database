5949eb14af9e366ea97823da1362a3436e02a3a2
Thomas Whittle <tewhittle@gmail.com>
2021-03-09 16:17:23 -0500

    Add header and table components

---
+++ src/EmployeeTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class EmployeeTable extends Component {
+    render() {
+        return (
+            <div className="tc">
+               <p>Table goes here</p>
+            </div>
+        )
+    }
+}
+
+export default EmployeeTable
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="tc bg-light-red pa4">
+                <h1>Employee Directory</h1>
+            </div>
+        )
+    }
+    
+}
+
+export default Header
