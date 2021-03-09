a4518d5bfcf850b999d1341ad35f1b1585464afc
Thomas Whittle <tewhittle@gmail.com>
2021-03-09 16:33:29 -0500

    Add basic table functionality to EmployeeTable component

--- src/EmployeeTable.js
+++ src/EmployeeTable.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react"
+import "./EmployeeTable.css"
 
 class EmployeeTable extends Component {
     render() {
         return (
-            <div className="tc">
-               <p>Table goes here</p>
+            <div>
+               <table>
+                   <caption>Employees</caption>
+                   <thead>
+                       <tr>
+                           <th>Full Name</th>
+                           <th>D.O.B.</th>
+                           <th>Email</th>
+                           <th>Phone</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       <td>Audrey Caufield</td>
+                       <td>02/29/92</td>
+                       <td>acaufield@gmail.com</td>
+                       <td>5555555555</td>
+                   </tbody>
+               </table>
             </div>
         )
     }
